77d8e6c11c2bf1cda07b729b95739d7bec98e50d
pranjalrai099 <pranjalkrai2004@gmail.com>
2025-02-27 03:38:49 +0530

    Update Contact.jsx

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { assets } from '../assets/assets_frontend/assets'
-
+import { DonationForm } from './DonationForm'
 const Contact = () => {
   return (
     <div>
       <div className='text-center text-2xl pt-10 text-gray-500'>
         <p>Contact <span className='text-gray-700 font-semibold'>US</span></p>
       </div>
-      <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28 text-sm'>
+      <div className='my-8 flex flex-col justify-center md:flex-row gap-10 mb-28 text-sm'>
         <img className='w-full md:max-w-[360px] ' src={assets.contact_image} alt=''/>
         <div className='flex flex-col justify-center items-start gap-5'>
           <p className='font-semibold text-lg text-gray-600 '>OUR OFFICE</p>
@@ -18,6 +18,7 @@ const Contact = () => {
           <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
         </div>
       </div>
+     <DonationForm/>
     </div>
   )
 }
